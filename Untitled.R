4739a450d5ce6797dad9b53e86d667da19017959
GregGU <ggur1373@gmail.com>
2018-04-27 17:34:11 -0400

    test

--- Untitled.R
+++ Untitled.R
@@ -1,10 +1,21 @@
-##########
-##
-Hi guys!
-  test
-########
-##########
-##########
-##########
-##########
-##########
+library(ggplot2)
+
+outputdir
+
+print_barplot <- function(xvar, fill, plotname){
+  pdf(plotname, width = 12, height = 7)
+  print(ggplot(spambase, aes(x = xvar,
+                     fill=factor(fill)
+  )) +
+  geom_bar(stat="bin")
+  )
+  dev.off()
+}
+
+
+getxvar.l <- colnames(spambase)
+for(l in 1:length(spambase)) {
+  if (getxvar.l[l] != "spam") # plot all predictors and exclude response "spam"
+  print_barplot(xvar=getxvar.l[l],fill=spam, plotname=paste0(outputdir,"/fig/barplot.pdf"))
+  else
+}
