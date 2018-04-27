72a427da20c1410f33805b636b29e47f1130ae21
GregGU <ggur1373@gmail.com>
2018-04-27 18:25:15 -0400

    stuff

--- messy.R
+++ messy.R
@@ -1,29 +1,24 @@
-library(ggplot2)
 
-workdir <- "C:/Users/greg/Dropbox/Jfinal_proj"
+
+
+workdir <- "C:/Users/greg/Documents/690FinalProject"
 setwd(workdir)
+runname <- "greg" # used to later to create a folder and directory for a specific run
+
 
 Rfiles <- list.files(file.path(paste0(getwd(),"/R/")), ".R") #gets names of r files
-#Rfiles <- Rfiles[grepl(".R", Rfiles)] # select R scripts in that folder
+  #Rfiles <- Rfiles[grepl(".R", Rfiles)] # select R scripts in that folder
 sapply(paste0(paste0(getwd(),"/R/"), Rfiles), source) #gets functions from file
   # make a output folder to store results from run
-  MakeDirs(runname)
-output.dir <- MakeDirs(runname)
+MakeDirs(runname)
+outputdir <- MakeDirs(runname)
 
-print_barplot <- function(xvar, fill, plotname){
-  pdf(plotname, width = 12, height = 7)
-  print(ggplot(spambase, aes(x = xvar,
-                     fill=factor(fill)
-  )) +
-  geom_bar(stat="bin")
-  )
-  dev.off()
-}
 
+
+library(ggplot2)
 
 getxvar.l <- colnames(spambase)
 for(l in 1:length(spambase)) {
   if (getxvar.l[l] != "spam") # plot all predictors and exclude response "spam"
-  print_barplot(xvar=getxvar.l[l],fill=spam, plotname=paste0(outputdir,"/fig/barplot.pdf"))
-  else
+  print_barplot(xvar=getxvar.l[l],fill="spam", plotname=paste0(outputdir,"/barplot.pdf"))
 }
