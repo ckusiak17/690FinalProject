0eee03f6a46c0a8a077b0567e0df04c173165c9b
GregGU <ggur1373@gmail.com>
2018-04-27 18:12:47 -0400

    changes inputs to strings

--- R/plotfunctions.R
+++ R/plotfunctions.R
@@ -1,7 +1,7 @@
 print_barplot <- function(xvar, fill, plotname){
   pdf(plotname, width = 12, height = 7)
-  print(ggplot(spambase, aes(x = xvar,
-                             fill=factor(fill)
+  print(ggplot(spambase, aes_string(x = xvar,
+                             fill=fill
   )) +
     geom_bar(stat="bin")
   )
