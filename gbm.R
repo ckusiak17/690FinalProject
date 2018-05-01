22d8749f9cc81a2abdd0390c2a53f9ccea32ca2f
GregGU <ggur1373@gmail.com>
2018-05-01 19:54:34 -0400

    final

--- gbm.R
+++ gbm.R
@@ -1,5 +1,5 @@
 
-#push error.....
+
 # for continuous
 train.size <- .8*nrow(temp2)
 rows <- 1:nrow(temp2)
@@ -35,8 +35,54 @@ summary(model_test)
 # Plot model
 plot(model_test)
 
+library(pROC)
 predictions<-predict(object = model_test, test.set, type='prob')
 auc <- roc(test.set.y, predictions[[2]])
 print(auc$auc)
 
 
+
+
+
+
+
+
+#############################################################
+###############         DUMMY TEST        #################
+############################################################
+
+
+train.size <- .8*nrow(dummy_data)
+rows <- 1:nrow(dummy_data)
+train.rows<-sample(rows, train.size, replace=F)
+test.rows<-subset(rows, !rows%in%train.rows)
+training.set <- dummy_data[train.rows,]
+training.set$spam <- NULL
+test.set<- dummy_data[test.rows,]
+test.set$spam<-NULL
+#for repsonse
+training.set.y <- spam[train.rows]
+test.set.y <- spam[test.rows]
+
+
+y<- as.factor(ifelse(training.set.y==0,'no','yes'))
+levels(y)
+
+
+
+model_test <- train(training.set, #just a normal data frame
+                    y, #has to be in yes or no format for some reason
+                    method = "gbm",
+                    metric = "ROC",
+                    trControl = trainControl(method = "cv", number = 10, summaryFunction =  twoClassSummary, classProbs = TRUE),
+                    preProcess = c("center", "scale")
+)
+
+
+summary(model_test)
+# Plot model
+plot(model_test)
+
+predictions<-predict(object = model_test, test.set, type='prob')
+auc <- roc(test.set.y, predictions[[2]])
+print(auc$auc)
